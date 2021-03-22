7160bf090b0c76d35939718ab0fd3b7a8c937c92
rpizarrog <rpizarro@itdurango.edu.mx>
2021-03-22 10:54:07 -0600

    Add files via upload

--- funciones/misfunciones.r
+++ funciones/misfunciones.r
@@ -70,3 +70,17 @@ f.moneda.dado <- function(S.espacio.muestral) {
   punto.muestral 
 }
 
+f.contar.dados <- function(S.espacio.muestral,inicial, final) {
+  for (i in 1:length(S.espacio.muestral)) {
+    sumas[i] <- as.numeric(substr(S.espacio.muestral[i],1,1)) + 
+      as.numeric(substr(S.espacio.muestral[i],2,2)) 
+  }
+  sumas <- sumas[order(sumas)]
+  print(sumas)
+  length(which(sumas >= inicial & sumas <=final))
+  
+}
+
+
+
+
