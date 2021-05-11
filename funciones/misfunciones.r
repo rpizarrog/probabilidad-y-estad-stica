f6aa880dd4c9392a9741040ae6513eacbf543944
rpizarrog <rpizarro@itdurango.edu.mx>
2021-05-11 14:59:06 -0500

    Update misfunciones.r

--- funciones/misfunciones.r
+++ funciones/misfunciones.r
@@ -144,6 +144,38 @@ plotunif <- function(x, min = 0, max = 1, lwd = 1, col = 1, ...) {
     points(max, min, pch = 21, col = col, bg = "white")
 }
 
+unif_area <- function(min = 0, max = 1, lb, ub, col = 1,
+                      acolor = "lightgray", ...) {
+    x <- seq(min - 0.25 * max, max + 0.25 * max, 0.001) 
+    
+    if (missing(lb)) {
+       lb <- min(x)
+    }
+    if (missing(ub)) {
+        ub <- max(x)
+    }
+    if(max < min) {
+        stop("'min' must be lower than 'max'")
+    }
+
+    x2 <- seq(lb, ub, length = 1000) 
+    plot(x, dunif(x, min = min, max = max),
+         xlim = c(min - 0.25 * max, max + 0.25 * max), type = "l",
+         ylab = "f(x)", lty = 0, ...)   
+
+    y <- dunif(x2, min = min, max = max)
+    polygon(c(lb, x2, ub), c(0, y, 0), col = acolor, lty = 0)
+    segments(min, 1/(max - min), max, 1/(max - min), lwd = 2, col = col)
+    segments(min - 2 * max, 0, min, 0, lwd = 2, col = col)
+    segments(max, 0, max + 2 * max, 0, lwd = 2, col = col)
+    points(min, 1/(max - min), pch = 19, col = col)
+    points(max, 1/(max - min), pch = 19, col = col)
+    segments(min, 0, min, 1/(max - min), lty = 2, col = col, lwd = 2)
+    segments(max, 0, max, 1/(max - min), lty = 2, col = col, lwd = 2)
+    points(0, min, pch = 21, col = col, bg = "white")
+    points(max, min, pch = 21, col = col, bg = "white")
+}
+
 
 
 
