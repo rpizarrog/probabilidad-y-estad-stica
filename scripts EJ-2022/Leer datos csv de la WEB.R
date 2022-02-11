fbedc7b2bd530d6bd52f875f7d9dcfa8ad59763e
rpizarrog <rpizarro@itdurango.edu.mx>
2022-02-11 10:31:01 -0600

    Add files via upload

---
+++ scripts EJ-2022/Leer datos csv de la WEB.R
@@ -0,0 +1,9 @@
+# Leer rchivo csv de WEB
+
+library(readr)
+
+
+localidades <- read.csv("https://raw.githubusercontent.com/rpizarrog/probabilidad-y-estad-stica/master/datos/locdurangomx.csv")
+
+datos.laboral <- read.csv("https://raw.githubusercontent.com/rpizarrog/probabilidad-y-estad-stica/master/datos/EncuestaClimaCulturaOrganizacional_2010-2017.csv")
+
