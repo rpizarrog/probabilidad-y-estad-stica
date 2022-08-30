a3212cc7d3270704917fc564ce2d873667f787d7
Rubén Pizarro Gurrola <rpizarro@itdurango.edu.mx>
2022-08-30 15:52:36 -0500

    Add files via upload

--- Agosto-Diciembre 2022/scripts/construir data frame a partir de vectores.r
+++ Agosto-Diciembre 2022/scripts/construir data frame a partir de vectores.r
@@ -1,7 +1,9 @@
 # Construir data frames a partir de vectores
-edades <- c(20, 19, 18, 18, 20, 19, 22, 23, 24, 25)
+edades <- c(20, 19, 18, 18, 20, 19, 22, 23, 24, 25, 20, 18, 19, 18, 18, 18, 19)
 nombres <- c("Carlos", "Jesus", "Marco", "Adriana", "Emiliano",
-             "Roberto", "Tairon", "Diego", "Bryan", "Tristan")
+             "Roberto", "Tairon", "Diego", "Bryan", "Tristan", 
+             "Arian", "Isaac", "Raymundo", "Rodrigo", "Oswaldo", 
+             "Abigail", "Alberto")
 
 datos <- data.frame(edades, nombres)
 datos
