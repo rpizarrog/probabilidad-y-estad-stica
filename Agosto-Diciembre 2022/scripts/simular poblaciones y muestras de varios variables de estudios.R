13cf0f8158796ee5b79a87e461640fbbe18af2fb
Rubén Pizarro Gurrola <rpizarro@itdurango.edu.mx>
2022-09-01 10:32:21 -0500

    Add files via upload

--- Agosto-Diciembre 2022/scripts/simular poblaciones y muestras de varios variables de estudios.R
+++ Agosto-Diciembre 2022/scripts/simular poblaciones y muestras de varios variables de estudios.R
@@ -1,5 +1,19 @@
-# Simular poblaciones
+# Simular poblaciones y muestras
 
-edades <- sample(x = 20:27, size = 6600, 
+# Población edades
+
+pob.edades <- sample(x = 20:27, size = 6600, 
                  replace = TRUE)
-edades
+pob.edades
+
+media.pob.edades <- mean(pob.edades)
+media.pob.edades
+
+# muestra edades
+
+muestra.edades <- sample(x = pob.edades, size = 660)
+muestra.edades
+
+media.muestra.edades <- mean(muestra.edades)
+media.muestra.edades
+
