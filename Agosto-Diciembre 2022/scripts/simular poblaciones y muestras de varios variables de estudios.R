4a935a798dfb38a12a2edda13cb3339e09ca8f38
Rubén Pizarro Gurrola <rpizarro@itdurango.edu.mx>
2022-09-01 10:37:38 -0500

    Add files via upload

--- Agosto-Diciembre 2022/scripts/simular poblaciones y muestras de varios variables de estudios.R
+++ Agosto-Diciembre 2022/scripts/simular poblaciones y muestras de varios variables de estudios.R
@@ -17,3 +17,22 @@ muestra.edades
 media.muestra.edades <- mean(muestra.edades)
 media.muestra.edades
 
+
+# OTRO EJEMPLO DE ... cALIFICACIONES DE ALUMNOS
+# RANGO ENTRE 70 A 100
+# Poblacion de calificaiones alumnos
+pob.calificaciones <- sample(x = 70:100, size = 6600, 
+                   replace = TRUE)
+pob.calificaciones
+
+media.califa <- mean(pob.calificaciones)
+media.califa
+
+# muestra edades
+
+muestra.califica <- sample(x = pob.calificaciones, size = 660)
+muestra.califica
+
+media.muestra.califica <- mean(muestra.califica)
+media.muestra.califica
+
